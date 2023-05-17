5b4deb36e97af65e1b4e4a8df6c8bf2744d492fa
Jeyanth <jeyanthdurai@gmail.com>
2023-05-17 10:26:11 +0530

    message

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,15 +1,12 @@
 package com.driver;
 
 public class Main {
-    public static void main(String args[]){
-        RWOnly obj=new RWOnly();
-        // System.out.println(obj.name="poovili");
-        //error
-        // C:\Users\admin\IdeaProjects\encapsulation-poovilikarthika\src\main\java\com\driver\Main.java:6:29
-        //java: name has private access in com.driver.RWOnly
 
-        obj.setName("Poovili");
-        System.out.println(obj.getName());
+    public static void main(String [] args){
+        RWOnly r=new RWOnly();
+        r.setName("Shraddha");
+        System.out.println(" Error: you might seen some error, please not it down in commnets");
+        r.getName();
 
     }
 }
